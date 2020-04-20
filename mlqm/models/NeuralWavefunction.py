89a6957301ff9abc2e9d633cedc4ed23814afaf8
Corey Adams <corey.adams@anl.gov>
2020-04-20 10:11:48 -0500

    Rename neural wavefunction

---
+++ mlqm/models/NeuralWavefunction.py
@@ -0,0 +1,75 @@
+import numpy
+import torch
+
+from .ExponentialBoundaryCondition import ExponentialBoundaryCondition
+
+class NeuralWavefunction(torch.nn.Module):
+    """Create a neural network eave function in N dimensions
+    
+    Boundary condition, if not supplied, is gaussian in every dimension
+    
+    Extends:
+        torch.nn.Module
+    """
+    def __init__(self, n : int, boundary_condition :torch.nn.Module = None):
+        torch.nn.Module.__init__(self)
+        
+        self.n = n
+        if self.n < 1 or self.n > 3: 
+            raise Exception("Dimension must be 1, 2, or 3 for NeuralWavefunction")
+
+        
+        # Normalization:
+        self.norm = 1.0
+        
+        # Create a boundary condition if needed:
+        if boundary_condition is None:
+            self.bc = ExponentialBoundaryCondition(self.n)
+        else:
+            self.bc = boundary_condition
+
+
+        self.layer1 = torch.nn.Linear(self.n, 32)
+        self.layer2 = torch.nn.Linear(32, 32)
+        self.layer3 = torch.nn.Linear(32, 1)
+    
+
+
+    def forward(self, inputs):
+        x = inputs
+        x = self.layer1(x)
+        x = torch.sigmoid(x)
+        x = self.layer2(x)
+        x = torch.sigmoid(x)
+        x = self.layer3(x)
+
+        print(x.shape)
+        print(self.bc)
+        boundary_condition = self.bc(inputs)
+
+
+        return self.norm*x*boundary_condition
+
+
+    def update_normalization(self, inputs, delta):
+        # Inputs is expected to be a range of parameters along an x axis.
+        with torch.no_grad():
+            value = self.forward(inputs)
+            N = value ** 2
+
+
+            N = torch.sum(N * delta)
+            self.norm *= 1/torch.sqrt(N)
+
+            # The normalization condition is that the integral of the wavefunction squared
+            # should be equal to 1 (probability sums to 1.)
+
+        return
+    
+    
+    def zero_grad(self):
+        """Sets gradients of all model parameters to zero."""
+        for p in self.parameters():
+            if p.grad is not None:
+                p.grad.data.zero_()
+
