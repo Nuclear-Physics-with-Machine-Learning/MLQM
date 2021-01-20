1733c84ac242764f2b985e6f3e84813872573fa7
Corey Adams <corey.adams@anl.gov>
2021-01-20 15:17:21 +0000

    Adjust MPI initialization

--- mlqm/__init__.py
+++ mlqm/__init__.py
@@ -7,3 +7,15 @@
 ELECTRON_CHARGE = 1.0
 
 DEFAULT_TENSOR_TYPE="float64"
+
+
+try:
+    import horovod.tensorflow as hvd
+    hvd.init()
+    MPI_AVAILABLE=True
+except:
+    MPI_AVAILABLE=False
+
+if MPI_AVAILABLE and hvd.size() == 1:
+    # Turn off mpi if only 1 rank
+    MPI_AVAILABLE = False
